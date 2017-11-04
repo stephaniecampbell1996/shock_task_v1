65ece9262769f296c01acb85762ea179bbea81c1
Stephanie Campbell <StephanieCampbell@Stephanies-MacBook-Pro-4.local>
2017-11-04 15:34:40 -0400

    Update database_connect.php

--- task/database_connect.php
+++ task/database_connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$dbc = mysql_connect('127.0.0.1', 'moral', 'j|n321');
-mysql_select_db('stephanie', $dbc);
+$dbc = mysql_connect('localhost', 'username', 'password');
+mysql_select_db('databasename', $dbc);
 
 ?>
